0b48bfd17a99439ae531adbfeea2718c3c5c8f31
evamittag <102155610+evamittag@users.noreply.github.com>
2022-03-24 16:28:35 +0100

    het

--- Delivery1.py
+++ Delivery1.py
@@ -1,3 +1,4 @@
+#Modules
 import dash
 from dash import dcc
 from dash import html
@@ -7,7 +8,7 @@ import pandas as pd
 
 #DATA import
 
-Excel_file = 'delivery/my_shop_data.xlsx'
+Excel_file = 'data/my_shop_data.xlsx'
 customers = pd.read_excel(Excel_file, "customers")
 order = pd.read_excel(Excel_file, "order")
 employee = pd.read_excel(Excel_file, "employee")
@@ -23,11 +24,3 @@ def order():
     fig = px.pie(order, values='Total', names='CompanyName', title='Top 5 Customers')
     return fig
 
-def employee():
-    fig = px.bar(employee, x='EmployeesName', y='Total', title='Sales by Employees')
-    return fig    
-
-def products():
-    fig = px.bar(products, x='CategoryName', y='Total', title='Category Sales')
-    return fig
-    
